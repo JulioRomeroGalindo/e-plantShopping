67b2b63183213b494fa84cd1d7b65c70f12e3806
JulioRomeroGalindo <juliocesarromerogalindo@gmail.com>
2025-04-02 17:02:19 -0400

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -12,10 +12,7 @@ function App() {
   
   const [showProductList, setShowProductList] = useState(false);
   const [showCart, setShowCart] = useState(false);
-  // Obtener la cantidad de elementos en el carrito desde Redux
-  const cartItems = useSelector((state) => state.cart.items);
-  const cartCount = cartItems.reduce((total, item) => total + item.quantity, 0);
-
+  
   const handleGetStartedClick = (e) => {
     if (e) e.preventDefault(); // Prevenir comportamiento por defecto si es necesario
     setShowProductList(true);
@@ -55,11 +52,7 @@ function App() {
           </div>
       
       </div>
-      <div className="header">
-          <button className="cart-button" onClick={handleViewCartClick}>
-            Cart ({cartCount})
-          </button>
-        </div>
+      
       <div className={`product-list-container ${showProductList ? 'visible' : ''}`}>
         <ProductList onHomeClick={handleHomeClick}/>
       </div>
